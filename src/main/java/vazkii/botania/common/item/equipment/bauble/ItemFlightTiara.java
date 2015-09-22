50386f545426c7c7897af0eef948df026e5895eb
kotasc3 <kotasc3636@gmail.com>
2015-09-22 10:44:33 -0400

    revert flugel tiara nerf

--- src/main/java/vazkii/botania/common/item/equipment/bauble/ItemFlightTiara.java
+++ src/main/java/vazkii/botania/common/item/equipment/bauble/ItemFlightTiara.java
@@ -19,8 +19,6 @@ import java.util.List;
 import javax.xml.bind.annotation.adapters.HexBinaryAdapter;
 
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.gui.Gui;
-import net.minecraft.client.gui.ScaledResolution;
 import net.minecraft.client.renderer.ItemRenderer;
 import net.minecraft.client.renderer.OpenGlHelper;
 import net.minecraft.client.renderer.Tessellator;
@@ -48,15 +46,11 @@ import vazkii.botania.api.item.IBaubleRender;
 import vazkii.botania.api.mana.IManaUsingItem;
 import vazkii.botania.api.mana.ManaItemHandler;
 import vazkii.botania.client.core.helper.IconHelper;
-import vazkii.botania.client.core.helper.RenderHelper;
 import vazkii.botania.client.core.helper.ShaderHelper;
 import vazkii.botania.client.lib.LibResources;
 import vazkii.botania.common.Botania;
 import vazkii.botania.common.achievement.ICraftAchievement;
 import vazkii.botania.common.achievement.ModAchievements;
-import vazkii.botania.common.core.helper.ItemNBTHelper;
-import vazkii.botania.common.core.helper.Vector3;
-import vazkii.botania.common.item.ModItems;
 import vazkii.botania.common.lib.LibItemNames;
 import baubles.api.BaubleType;
 import baubles.common.lib.PlayerHandler;
@@ -68,19 +62,10 @@ import cpw.mods.fml.relauncher.SideOnly;
 
 public class ItemFlightTiara extends ItemBauble implements IManaUsingItem, IBaubleRender, ICraftAchievement {
 
-	private static ResourceLocation textureHud = new ResourceLocation(LibResources.GUI_HUD_ICONS);
 	private static ResourceLocation textureHalo = new ResourceLocation(LibResources.MISC_HALO);
-
-	private static final String TAG_FLYING = "flying";
-	private static final String TAG_TIME_LEFT = "timeLeft";
-	private static final String TAG_INFINITE_FLIGHT = "infiniteFlight";
-	private static final String TAG_DASH_COOLDOWN = "dashCooldown";
-	private static final String TAG_IS_SPRINTING = "isSprinting";
 
 	public static List<String> playersWithFlight = new ArrayList();
 	private static final int COST = 35;
-	private static final int COST_OVERKILL = COST * 3;
-	private static final int MAX_FLY_TIME = 400;
 
 	public static IIcon[] wingIcons;
 	private static final int SUBTYPES = 8;
@@ -159,77 +144,18 @@ public class ItemFlightTiara extends ItemBauble implements IManaUsingItem, IBaub
 		return String.copyValueOf(chrs);
 	}
 
-	@Override
-	public void onWornTick(ItemStack stack, EntityLivingBase player) {
-		super.onWornTick(stack, player);
-
-		if(player instanceof EntityPlayer) {
-			EntityPlayer p = (EntityPlayer) player;
-			boolean flying = p.capabilities.isFlying;
-
-			boolean wasSprting = ItemNBTHelper.getBoolean(stack, TAG_IS_SPRINTING, false);
-			boolean isSprinting = p.isSprinting();
-			if(isSprinting != wasSprting)
-				ItemNBTHelper.setBoolean(stack, TAG_IS_SPRINTING, isSprinting);
-
-			int time = ItemNBTHelper.getInt(stack, TAG_TIME_LEFT, MAX_FLY_TIME);
-			int newTime = time;
-			Vector3 look = new Vector3(p.getLookVec());
-			look.y = 0;
-			look.normalize();
-
-			if(flying) {
-				if(time > 0 && !ItemNBTHelper.getBoolean(stack, TAG_INFINITE_FLIGHT, false))
-					newTime--;
-				final int maxCd = 80;
-				int cooldown = ItemNBTHelper.getInt(stack, TAG_DASH_COOLDOWN, 0);
-				if(!wasSprting && isSprinting && cooldown == 0) {
-					p.motionX += look.x;
-					p.motionZ += look.z;
-					p.worldObj.playSoundAtEntity(p, "botania:dash", 1F, 1F);
-					ItemNBTHelper.setInt(stack, TAG_DASH_COOLDOWN, maxCd);
-				} else if(cooldown > 0) {
-					if(maxCd - cooldown < 2)
-						player.moveFlying(0F, 1F, 5F);
-					else if(maxCd - cooldown < 10)
-						player.setSprinting(false);
-					ItemNBTHelper.setInt(stack, TAG_DASH_COOLDOWN, cooldown - 2);
-					if(player instanceof EntityPlayerMP)
-						BotaniaAPI.internalHandler.sendBaubleUpdatePacket((EntityPlayerMP) player, 0);
-				}
-			} else if(!flying) {
-				boolean doGlide = player.isSneaking() && !player.onGround;
-				if(time < MAX_FLY_TIME && player.ticksExisted % (doGlide ? 6 : 2) == 0)
-					newTime++;
-
-				if(doGlide) {
-					player.motionY = Math.max(-0.15F, player.motionY);
-					float mul = 0.6F;
-					player.motionX = look.x * mul;
-					player.motionZ = look.z * mul;
-					player.fallDistance = 0F;
-				}
-			}
-
-			ItemNBTHelper.setBoolean(stack, TAG_FLYING, flying);
-			if(newTime != time)
-				ItemNBTHelper.setInt(stack, TAG_TIME_LEFT, newTime);
-		}
-	}
-
 	@SubscribeEvent
 	public void updatePlayerFlyStatus(LivingUpdateEvent event) {
 		if(event.entityLiving instanceof EntityPlayer) {
 			EntityPlayer player = (EntityPlayer) event.entityLiving;
-			ItemStack tiara = PlayerHandler.getPlayerBaubles(player).getStackInSlot(0);
-			int left = ItemNBTHelper.getInt(tiara, TAG_TIME_LEFT, MAX_FLY_TIME);
 
+			ItemStack tiara = PlayerHandler.getPlayerBaubles(player).getStackInSlot(0);
 			if(playersWithFlight.contains(playerStr(player))) {
 				if(shouldPlayerHaveFlight(player)) {
 					player.capabilities.allowFlying = true;
 					if(player.capabilities.isFlying) {
 						if(!player.worldObj.isRemote)
-							ManaItemHandler.requestManaExact(tiara, player, getCost(tiara, left), true);
+							ManaItemHandler.requestManaExact(tiara, player, COST, true);
 						else if(Math.abs(player.motionX) > 0.1 || Math.abs(player.motionZ) > 0.1) {
 							double x = event.entityLiving.posX - 0.5;
 							double y = event.entityLiving.posY - 1.7;
@@ -320,17 +246,7 @@ public class ItemFlightTiara extends ItemBauble implements IManaUsingItem, IBaub
 
 	private boolean shouldPlayerHaveFlight(EntityPlayer player) {
 		ItemStack armor = PlayerHandler.getPlayerBaubles(player).getStackInSlot(0);
-		if(armor != null && armor.getItem() == this) {
-			int left = ItemNBTHelper.getInt(armor, TAG_TIME_LEFT, MAX_FLY_TIME);
-			boolean flying = ItemNBTHelper.getBoolean(armor, TAG_FLYING, false);
-			return (left > (flying ? 0 : MAX_FLY_TIME / 10) || player.inventory.hasItem(ModItems.flugelEye)) && ManaItemHandler.requestManaExact(armor, player, getCost(armor, left), false);
-		}
-
-		return false;
-	}
-
-	public int getCost(ItemStack stack, int timeLeft) {
-		return timeLeft <= 0 ? COST_OVERKILL : COST;
+		return armor != null && armor.getItem() == this && ManaItemHandler.requestManaExact(armor, player, COST, false);
 	}
 
 	@Override
@@ -496,51 +412,6 @@ public class ItemFlightTiara extends ItemBauble implements IManaUsingItem, IBaub
 		GL11.glShadeModel(GL11.GL_FLAT);
 		GL11.glEnable(GL11.GL_CULL_FACE);
 	}
-
-	@SideOnly(Side.CLIENT)
-	public static void renderHUD(ScaledResolution resolution, EntityPlayer player, ItemStack stack) {
-		int u = Math.max(1, stack.getItemDamage()) * 9 - 9;
-		int v = 0;
-
-		Minecraft mc = Minecraft.getMinecraft();
-		mc.renderEngine.bindTexture(textureHud);
-		int xo = resolution.getScaledWidth() / 2 + 10;
-		int x = xo;
-		int y = resolution.getScaledHeight() - 49;
-		if(player.getAir() < 300)
-			y -= 10;
-
-		int left = ItemNBTHelper.getInt(stack, TAG_TIME_LEFT, MAX_FLY_TIME);
-
-		int segTime = MAX_FLY_TIME / 10;
-		int segs = left / segTime + 1;
-		int last = left % segTime;
-
-		for(int i = 0; i < segs; i++) {
-			float trans = 1F;
-			if(i == segs - 1) {
-				trans = (float) last / (float) segTime;
-				GL11.glEnable(GL11.GL_BLEND);
-				GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
-				GL11.glDisable(GL11.GL_ALPHA_TEST);
-			}
-
-			GL11.glColor4f(1F, 1F, 1F, trans);
-			RenderHelper.drawTexturedModalRect(x, y, 0, u, v, 9, 9);
-			x += 8;
-		}
-
-		if(player.capabilities.isFlying) {
-			int width = ItemNBTHelper.getInt(stack, TAG_DASH_COOLDOWN, 0);
-			GL11.glColor4f(1F, 1F, 1F, 1F);
-			if(width > 0)
-				Gui.drawRect(xo, y - 2, xo + 80, y - 1, 0x88000000);
-			Gui.drawRect(xo, y - 2, xo + width, y - 1, 0xFFFFFFFF);
-		}
-
-		GL11.glEnable(GL11.GL_ALPHA_TEST);
-		GL11.glColor4f(1F, 1F, 1F, 1F);
-		mc.renderEngine.bindTexture(Gui.icons);
 	}
 
 	@Override
