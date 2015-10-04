6a350260a099fdf2f082b28accfa1a9598f246f1
kotasc3 <kotasc3636@gmail.com>
2015-10-04 01:53:11 -0400

    website commit

--- README.md
+++
@@ -1,3 +0,0 @@
-feel free to add this to any sort of pack if you must
-
-licensed under the [Botania License](http://botaniamod.net/license.php)
--- src/main/java/vazkii/botania/api/package-info.java
+++
@@ -1,4 +0,0 @@
-@API(owner = "Botania", apiVersion = "64", provides = "BotaniaAPI")
-package vazkii.botania.api;
-import cpw.mods.fml.common.API;
-
